e2c5063952945a953baa014aced371c99bb28581
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2024-04-11 11:41:04 +0200

    steg2

--- index.js
+++ index.js
@@ -1,39 +1,113 @@
 let pokemonsArr;
 let dropdownOption;
-let data ;
-let dropdownLista = document.createElement("select");
-document.body.append(dropdownLista);
+let dropdownList=document.getElementById("pokemon-dropdown");
+let pokemonDetails = document.getElementById("Pokemon-details");
 
+
+async function fetchData(){
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+    let response = await fetch (url)
+    let data = await response.json();
+    pokemonsArr=data.results;
+    createDropdownOptions(pokemonsArr);
+}
 
 function createDropdownOptions(pokemonsArr) {
-    pokemonsArr.forEach(pokemon => {
+    pokemonsArr.forEach( pokemon => {
         dropdownOption=document.createElement("option");
-        dropdownOption.value=pokemon.name;
+        dropdownOption.value=pokemon.url;
         dropdownOption.innerText=pokemon.name;
-        dropdownLista.appendChild(dropdownOption)
+        dropdownList.appendChild(dropdownOption)
     });
 };
 
-async function fetchData(){
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-     data = await response.json();
-    // console.log(data.results);
-    pokemonsArr=data.results;
-   createDropdownOptions(pokemonsArr);
-};
-
-
-
-
 fetchData();
 
+class Pokemon {
+    constructor(name, image, types, weight, height, stats){
+        this.name=name;
+        this.image=image;
+        this.types=types;
+        this.weight=weight;
+        this.height=height;
+        this.stats=stats;
+    }
+    comparePokemones(){
+    }
+}
 
 
 
+//fetch selected pokemon
+
+async function createPokemonInstance (pokemonData){
+    const pokemonInstance = new Pokemon(
+        pokemonData.name,
+        pokemonData.sprites.front_default,
+        pokemonData.types.map(typeInfo => typeInfo.type.name),
+        pokemonData.weight,
+        pokemonData.height,
+        {
+            hp: pokemonData.stats[0].base_stat,
+            attack: pokemonData.stats[1].base_stat,
+            defense: pokemonData.stats[2].base_stat,
+            specialAttack: pokemonData.stats[3].base_stat,
+            specialDefense: pokemonData.stats[4].base_stat,
+            speed: pokemonData.stats[5].base_stat,
+        }
+    );
+    return pokemonInstance;
+    // console.log(pokemonInstance);
+
+}
+
+
+async function fetchSelectedPokemon(selectedPokemonUrl){
+    let pokemonResponse = await fetch (selectedPokemonUrl)
+    let pokemonData= await pokemonResponse.json();
+    let pokemonInstance = await createPokemonInstance(pokemonData);
+    await renderPokemon(pokemonDetails, pokemonInstance);
+}
+
+
+dropdownList.addEventListener("change", async ()=>{
+    let selectedPokemonUrl=dropdownList.value;
+    await fetchSelectedPokemon(selectedPokemonUrl);
+   
+});
+
+
+async function renderPokemon(pokemonDetails, pokemonInstance){
+    pokemonDetails.innerText="";
+
+    const pokemonName = document.createElement("h2");
+    pokemonName.innerText=pokemonInstance.name;
+
+    const pokemonImage= document.createElement("img");
+    pokemonImage.src=pokemonInstance.image;
+
+    const pokemonTypes=document.createElement("p");
+    pokemonTypes.innerText=pokemonInstance.types;
+
+    const pokemonWeight = document.createElement('p');
+    pokemonWeight.innerText=pokemonInstance.weight;
+
+    const pokemonHeight = document.createElement('p');
+    pokemonHeight.innerText =pokemonInstance.height;
 
 
 
-//make an array of nameproperty
+    pokemonDetails.appendChild(pokemonName);
+    pokemonDetails.appendChild(pokemonImage);
+    pokemonDetails.appendChild(pokemonTypes);
+    pokemonDetails.appendChild(pokemonWeight);
+    pokemonDetails.appendChild(pokemonHeight);
+
+    
+}
+
+
+
 
 
 
